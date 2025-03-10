c125af0dfd0d831767bdd11b9d52f4afd1e98ee8
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-03-10 12:46:13 +0000

    Implement home VM in controller

--- Website/Controllers/HomeController.cs
+++ Website/Controllers/HomeController.cs
@@ -1,21 +1,30 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Website.Data;
 using Website.Models;
+using Website.Models.ViewModels;
 
 namespace Website.Controllers;
 
-public class HomeController : Controller
+public class HomeController(ILogger<HomeController> logger, ApplicationDbContext context) : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+    private readonly ILogger<HomeController> _logger = logger;
 
     public IActionResult Index()
     {
-        return View();
+        var reviews = context.Reviews
+            .AsNoTracking()
+            .Include(r => r.Game)
+            .Take(25)
+            .ToList();
+
+        var homeVM = new HomeViewModel
+        {
+            Reviews = reviews,
+        };
+
+        return View(homeVM);
     }
 
     public IActionResult Privacy()
