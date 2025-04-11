e9795c4a8456734c8a3b7a70d28382419367de0b
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-04-11 17:14:00 +0100

    Add external logins

--- Website/Program.cs
+++ Website/Program.cs
@@ -1,4 +1,3 @@
-using IGDB;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Website.Data;
@@ -16,10 +15,37 @@ public class Program
                                throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
         builder.Services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(connectionString));
-        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+        if (builder.Environment.IsDevelopment())
+        {
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+        }
 
         builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
             .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        builder.Services.AddAuthentication()
+            .AddGoogle(options =>
+            {
+                options.ClientId = builder.Configuration["Authentication:Google:ClientId"]
+                                   ?? throw new InvalidOperationException("Could not find Google Client ID");
+                options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"]
+                                       ?? throw new InvalidOperationException("Could not find Google Client Secret");
+            })
+            .AddFacebook(options =>
+            {
+                options.ClientId = builder.Configuration["Authentication:Facebook:AppId"]
+                                   ?? throw new InvalidOperationException("Could not find Facebook App ID");
+                options.ClientSecret = builder.Configuration["Authentication:Facebook:AppSecret"]
+                                       ?? throw new InvalidOperationException("Could not find Facebook App Secret");
+            })
+            .AddTwitter(options =>
+            {
+                options.ConsumerKey = builder.Configuration["Authentication:Twitter:ConsumerAPIKey"]
+                                      ?? throw new InvalidOperationException("Could not find Twitter Consumer Key");
+                options.ConsumerSecret = builder.Configuration["Authentication:Twitter:ConsumerSecret"]
+                                         ?? throw new InvalidOperationException("Could not find Twitter Consumer Secret");
+            });
+
         var mvcBuilder = builder.Services.AddControllersWithViews();
 
         if (builder.Environment.IsDevelopment())
