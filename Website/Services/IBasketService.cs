11008d3870441ced177ca82b3c1a8c1581b9e1b5
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-04-18 15:43:06 +0100

    Add documentation to basket service methods

--- Website/Services/IBasketService.cs
+++ Website/Services/IBasketService.cs
@@ -4,8 +4,19 @@ namespace Website.Services;
 
 public interface IBasketService
 {
+    /// <summary>
+    /// Get the count of products in the basket.
+    /// </summary>
+    /// <param name="httpContext">HttpContext object</param>
+    /// <returns></returns>
     Task<int> ProductCount(HttpContext httpContext);
 
+    /// <summary>
+    /// Add a game to a basket. Or increase its count.
+    /// </summary>
+    /// <param name="httpContext">HttpContext object</param>
+    /// <param name="gameId">ID of the game to be added</param>
+    /// <returns></returns>
     Task Add(HttpContext httpContext, long gameId);
 
     /// <summary>
