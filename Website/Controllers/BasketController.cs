31ccca48e10267ccb7b6346cdcedf6e0ef135eb2
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-04-18 19:04:03 +0100

    Add total price to model

--- Website/Controllers/BasketController.cs
+++ Website/Controllers/BasketController.cs
@@ -9,14 +9,15 @@ namespace Website.Controllers;
 [Route("[controller]")]
 public class BasketController(IBasketService basketService) : Controller
 {
-    private readonly IBasketService _basketService = basketService;
+    private readonly IBasketService _basket = basketService;
 
     // GET
     public async Task<IActionResult> Index()
     {
         var model = new ViewBasketViewModel
         {
-            Items = await _basketService.Items()
+            Items = await _basket.Items(),
+            TotalPrice = await _basket.TotalPrice()
         };
         return View(model);
     }
@@ -25,7 +26,7 @@ public class BasketController(IBasketService basketService) : Controller
     public async Task<IActionResult> AddToBasket(int gameId)
     {
         var returnUrl = Request.GetTypedHeaders().Referer?.PathAndQuery ?? "/";
-        await _basketService.Add(gameId);
+        await _basket.Add(gameId);
         return Redirect(returnUrl);
     }
 }
--- Website/Models/ViewModels/ViewBasketViewModel.cs
+++ Website/Models/ViewModels/ViewBasketViewModel.cs
@@ -2,5 +2,7 @@ namespace Website.Models.ViewModels;
 
 public class ViewBasketViewModel
 {
+    public required decimal TotalPrice { get; set; }
+
     public required IList<BasketItem> Items { get; set; }
 }
