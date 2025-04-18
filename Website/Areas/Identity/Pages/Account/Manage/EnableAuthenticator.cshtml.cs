7404f3f878ad8b65756ebf092e800121c2ea7c07
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-04-18 20:41:52 +0100

    Use name of the website in the generated QR code for MFA

--- Website/Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
+++ Website/Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
@@ -181,7 +181,7 @@ namespace Website.Areas.Identity.Pages.Account.Manage
             return string.Format(
                 CultureInfo.InvariantCulture,
                 AuthenticatorUriFormat,
-                _urlEncoder.Encode("Microsoft.AspNetCore.Identity.UI"),
+                _urlEncoder.Encode("Re:Views"),
                 _urlEncoder.Encode(email),
                 unformattedKey);
         }
