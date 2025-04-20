e27f75540c82c843a559555b00c64655720c30b6
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-04-20 16:42:16 +0100

    Refactor reviews admin controller

--- Website/Controllers/ReviewsAdmin.cs
+++ Website/Controllers/ReviewsAdmin.cs
@@ -1,157 +1,144 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Website.Data;
 using Website.Models;
 
-namespace Website.Controllers
+namespace Website.Controllers;
+
+public class ReviewsAdmin(ApplicationDbContext context) : Controller
 {
-    public class ReviewsAdmin : Controller
+    // GET: ReviewsAdmin
+    public async Task<IActionResult> Index()
     {
-        private readonly ApplicationDbContext _context;
+        return View(await context.Reviews.ToListAsync());
+    }
 
-        public ReviewsAdmin(ApplicationDbContext context)
+    // GET: ReviewsAdmin/Details/5
+    public async Task<IActionResult> Details(long? id)
+    {
+        if (id == null)
         {
-            _context = context;
+            return NotFound();
         }
 
-        // GET: ReviewsAdmin
-        public async Task<IActionResult> Index()
+        var review = await context.Reviews
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (review == null)
         {
-            return View(await _context.Reviews.ToListAsync());
+            return NotFound();
         }
 
-        // GET: ReviewsAdmin/Details/5
-        public async Task<IActionResult> Details(long? id)
+        return View(review);
+    }
+
+    // GET: ReviewsAdmin/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: ReviewsAdmin/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Title,Body,Rating")] Review review)
+    {
+        if (ModelState.IsValid)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var review = await _context.Reviews
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (review == null)
-            {
-                return NotFound();
-            }
-
-            return View(review);
-        }
-
-        // GET: ReviewsAdmin/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: ReviewsAdmin/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,Body,Rating")] Review review)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(review);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(review);
-        }
-
-        // GET: ReviewsAdmin/Edit/5
-        public async Task<IActionResult> Edit(long? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var review = await _context.Reviews.FindAsync(id);
-            if (review == null)
-            {
-                return NotFound();
-            }
-            return View(review);
-        }
-
-        // POST: ReviewsAdmin/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(long id, [Bind("Id,Title,Body,Rating")] Review review)
-        {
-            if (id != review.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(review);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ReviewExists(review.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(review);
-        }
-
-        // GET: ReviewsAdmin/Delete/5
-        public async Task<IActionResult> Delete(long? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var review = await _context.Reviews
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (review == null)
-            {
-                return NotFound();
-            }
-
-            return View(review);
-        }
-
-        // POST: ReviewsAdmin/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(long id)
-        {
-            var review = await _context.Reviews.FindAsync(id);
-            if (review != null)
-            {
-                _context.Reviews.Remove(review);
-            }
-
-            await _context.SaveChangesAsync();
+            context.Add(review);
+            await context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+        return View(review);
+    }
 
-        private bool ReviewExists(long id)
+    // GET: ReviewsAdmin/Edit/5
+    public async Task<IActionResult> Edit(long? id)
+    {
+        if (id == null)
         {
-            return _context.Reviews.Any(e => e.Id == id);
+            return NotFound();
         }
+
+        var review = await context.Reviews.FindAsync(id);
+        if (review == null)
+        {
+            return NotFound();
+        }
+        return View(review);
+    }
+
+    // POST: ReviewsAdmin/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(long id, [Bind("Id,Title,Body,Rating")] Review review)
+    {
+        if (id != review.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                context.Update(review);
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReviewExists(review.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(review);
+    }
+
+    // GET: ReviewsAdmin/Delete/5
+    public async Task<IActionResult> Delete(long? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var review = await context.Reviews
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (review == null)
+        {
+            return NotFound();
+        }
+
+        return View(review);
+    }
+
+    // POST: ReviewsAdmin/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(long id)
+    {
+        var review = await context.Reviews.FindAsync(id);
+        if (review != null)
+        {
+            context.Reviews.Remove(review);
+        }
+
+        await context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool ReviewExists(long id)
+    {
+        return context.Reviews.Any(e => e.Id == id);
     }
 }
