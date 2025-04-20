a2b3c8e19a3a2183090ed6eeb32ce1cce3e5a3a2
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-04-20 17:10:18 +0100

    Add action to assign admin user the admin role

--- Website/Controllers/HomeController.cs
+++ Website/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Website.Data;
@@ -7,9 +8,17 @@ using Website.Models.ViewModels;
 
 namespace Website.Controllers;
 
-public class HomeController(ILogger<HomeController> logger, ApplicationDbContext context) : Controller
+public class HomeController(
+    ILogger<HomeController> logger,
+    ApplicationDbContext context,
+    RoleManager<IdentityRole> roleManager,
+    UserManager<User> userManager) : Controller
 {
     private readonly ILogger<HomeController> _logger = logger;
+
+    private readonly RoleManager<IdentityRole> _roleManager = roleManager;
+
+    private readonly UserManager<User> _userManager = userManager;
 
     public IActionResult Index()
     {
@@ -31,6 +40,27 @@ public class HomeController(ILogger<HomeController> logger, ApplicationDbContext
     public IActionResult Privacy()
     {
         return View();
+    }
+
+    public async Task<IActionResult> CreateRolesAndUser()
+    {
+        const string adminUserEmail = "";
+        var adminExists = await _roleManager.RoleExistsAsync("Admin");
+        if (!adminExists)
+        {
+            await _roleManager.CreateAsync(new IdentityRole("Admin"));
+        }
+
+        var user = await _userManager.FindByEmailAsync(adminUserEmail);
+        if (user != null)
+        {
+            if (!await _userManager.IsInRoleAsync(user, "Admin"))
+            {
+                await _userManager.AddToRoleAsync(user, "Admin");
+            }
+        }
+
+        return RedirectToAction(nameof(Index));
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
