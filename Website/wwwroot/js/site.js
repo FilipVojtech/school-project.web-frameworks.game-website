af37a0dd747d17726951d2e16f70b0f3fcc76158
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-04-28 14:41:12 +0100

    Add togglePassword function

--- Website/wwwroot/js/site.js
+++ Website/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+/**
+ * Toggle showing contents of a password field
+ * @param buttonEl The button element being clicked
+ * @param fieldSelector The password field being toggled
+ */
+function togglePassword(buttonEl, fieldSelector) {
+    const passwordField = $(fieldSelector)[0];
+    if (passwordField.type === "password") {
+        buttonEl.innerHTML = "<i class=\"bi bi-eye-slash\"></i>";
+        passwordField.type = ("text");
+    } else {
+        buttonEl.innerHTML = "<i class=\"bi bi-eye\"></i>";
+        passwordField.type = ("password");
+    }
+}
